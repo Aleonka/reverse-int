09227f53e4be3f00c6f558e3f0caa4962f0dcdd3
Aleonka <70817157+Aleonka@users.noreply.github.com>
2020-10-06 00:50:12 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+let p = String(n);
+let b = "";
+for (let i = p.length -1; i >= 0; i--) {
+ b = b + p[i];
 }
+let result = Number(b);
+return result;
+};
